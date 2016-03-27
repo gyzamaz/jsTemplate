8af6062868723b87394a6fcb584a2b90cebf8de4
gyzamaz <konradkadzielawa@gmail.com>
2016-03-28 00:18:55 +0200

    krystian

---
+++ tmp/tydzien2.php
@@ -0,0 +1,5 @@
+BoldPopup
+- implementacja metod show i hide w boldPopup
+- dodawanie instancji popup’u do wskazanego elementu nadrzędnego
+- dodanie wyświetlania preloader’a
+- zliczanie dodanych preloader’ów w obrębie elementów nadrzędnych
